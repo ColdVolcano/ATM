16b011bfee1a37b081aebbfbc909c6c3bc6ff770
ColdVolcano <mineflamecraft@hotmail.com>
2019-08-22 23:25:17 -0500

    Agregar archivos de proyecto.

---
+++ Cajero.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Cajero
+    {
+        private static int billetes20Disponibles = 500;
+        public static void IniciarServicio()
+        {
+            while (true)
+            {
+                escribirCentrado("Bienvenido, ingrese su numero de cuenta para continuar", 5);
+                Console.CursorVisible = true;
+                string intentoCuenta = obtenerNumero(5);
+                Console.CursorVisible = false;
+                if (Cuenta.VerificarCuenta(intentoCuenta))
+                {
+                    Cuenta usuario;
+                    if ((usuario = autenticarUsuario(intentoCuenta)) != null)
+                        iniciarSesion(usuario);
+                    else
+                        escribirCentrado("No se pudo autenticar, regresando a la pagina principal");
+                }
+                else
+                {
+                    escribirCentrado("La cuenta ingresada no existe, intente nuevamente");
+                }
+
+                Task.Delay(4000).Wait();
+            }
+        }
+
+        private static void escribirCentrado(string mensaje, int espaciosEntrada = 0, int offsetVertical = 0)
+        {
+            Console.ResetColor();
+            if (offsetVertical == 0)
+                Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - mensaje.Length) / 2, Console.WindowHeight / 2 - 1 + offsetVertical);
+            Console.WriteLine(mensaje);
+            if (espaciosEntrada > 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition((Console.WindowWidth - espaciosEntrada) / 2, Console.WindowHeight / 2 + 1 + offsetVertical);
+                for (int i = 0; i < espaciosEntrada; i++)
+                    Console.Write(' ');
+                Console.CursorLeft = (Console.WindowWidth - espaciosEntrada) / 2;
+            }
+        }
+
+        private static void imprimirOpciones(string[] opciones)
+        {
+            for (int i = 0; i < opciones.Length; i++)
+            {
+                Console.SetCursorPosition(i % 2 == 0 ? 0 : Console.WindowWidth - (opciones[i] + (i + 1)).Length - 3, Console.WindowHeight / 2 + i / 2 + 1);
+                if (i % 2 == 0)
+                    Console.Write($"[" + (i + 1) + "]-");
+                Console.Write(opciones[i]);
+                if (i % 2 != 0)
+                    Console.Write($"-[" + (i + 1) + "]");
+            }
+        }
+
+        private static string obtenerNumero(int longitud, bool escribir = true, bool retardo = true)
+        {
+            string retorno = "";
+            bool longitudMax = longitud < 0;
+            longitud = Math.Abs(longitud);
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+
+            while (longitud > 0)
+            {
+                char tecla;
+                if (char.IsDigit(tecla = Console.ReadKey(true).KeyChar))
+                {
+                    retorno += tecla;
+                    if (escribir)
+                        Console.Write(tecla);
+                    longitud--;
+                }
+
+                if (longitudMax && tecla == '\r')
+                    break;
+            }
+
+            if (retardo)
+                Task.Delay(120).Wait();
+
+            return retorno;
+        }
+
+        private static Cuenta autenticarUsuario(string usuario)
+        {
+            escribirCentrado("Introduzca su NIP a 5 digitos", 5);
+            Console.CursorVisible = true;
+            Console.CursorVisible = false;
+            return Cuenta.VerificarCuenta(usuario, obtenerNumero(5));
+        }
+
+        private static string[] opcionesPrincipal =
+        {
+            "Consulta de saldo",
+            "Retiro",
+            "Deposito",
+            "Salir del sistema"
+        };
+
+        private static void iniciarSesion(Cuenta cuenta)
+        {
+            bool write = true;
+            while (true)
+            {
+                if (write)
+                {
+                    escribirCentrado("Bienvenido, elija una opción");
+                    imprimirOpciones(opcionesPrincipal);
+                }
+
+                write = true;
+
+                switch (obtenerNumero(1, false))
+                {
+                    case "1":
+                        consultaCuenta(cuenta);
+                        break;
+                    case "2":
+                        retiroCuenta(cuenta);
+                        break;
+                    case "3":
+                        depositoCuenta(cuenta);
+                        break;
+                    case "4":
+                        escribirCentrado("Gracias por su preferencia");
+                        cuenta.CerrarSesion();
+                        return;
+                    default:
+                        write = false;
+                        break;
+                }
+            }
+        }
+
+        private static void consultaCuenta(Cuenta cuenta)
+        {
+            escribirCentrado("Su saldo es de " + cuenta.Saldo);
+            escribirCentrado("Presione 0 para volver al menú principal", offsetVertical: 2);
+            while (obtenerNumero(1, false) != "0") ;
+        }
+
+        private static string[] opcionesRetiro =
+        {
+            "$20",
+            "$40",
+            "$60",
+            "$100",
+            "$200",
+            "Regresar",
+        };
+
+        private static void retiroCuenta(Cuenta cuenta)
+        {
+            bool write = true;
+            while (true)
+            {
+                if (write)
+                {
+                    escribirCentrado("¿Qué cantidad desea retirar?");
+                    imprimirOpciones(opcionesRetiro);
+                }
+
+                write = true;
+
+                switch (obtenerNumero(1, false))
+                {
+                    case "1":
+                        if (procesarRetiro(cuenta, 20))
+                            return;
+                        else
+                            break;
+                    case "2":
+                        if (procesarRetiro(cuenta, 40))
+                            return;
+                        else
+                            break;
+                    case "3":
+                        if (procesarRetiro(cuenta, 60))
+                            return;
+                        else
+                            break;
+                    case "4":
+                        if (procesarRetiro(cuenta, 100))
+                            return;
+                        else
+                            break;
+                    case "5":
+                        if (procesarRetiro(cuenta, 200))
+                            return;
+                        else
+                            break;
+                    case "6":
+                        return;
+                    default:
+                        write = false;
+                        break;
+                }
+            }
+        }
+
+        private static bool procesarRetiro(Cuenta cuenta, float cantidad)
+        {
+            if (cuenta.Saldo < cantidad)
+            {
+                escribirCentrado("No cuenta con el saldo suficiente para esta operación");
+                escribirCentrado("Seleccione una cantidad menor", offsetVertical: 2);
+                Task.Delay(4000).Wait();
+                return false;
+            }
+            if (billetes20Disponibles * 20 < cantidad)
+            {
+                escribirCentrado("El cajero no cuenta con suficiente dinero para realizar la operación");
+                escribirCentrado("Seleccione una cantidad menor", offsetVertical: 2);
+                Task.Delay(4000).Wait();
+                return false;
+            }
+            cuenta.Retiro(cantidad);
+            billetes20Disponibles -= (int)cantidad / 20;
+
+            escribirCentrado("Recoge tu efectivo");
+            escribirCentrado("(Presiona cualquier numero)", offsetVertical: 1);
+
+            escribirCentrado("/                             \\", offsetVertical: 3);
+            escribirCentrado("/                               \\", offsetVertical: 4);
+            escribirCentrado("-----------------------------------", offsetVertical: 5);
+
+            obtenerNumero(1, false);
+
+            return true;
+        }
+
+        private static void depositoCuenta(Cuenta cuenta)
+        {
+            escribirCentrado("Digite la cantidad a depositar");
+            escribirCentrado("O digite 0 para regresar al menú", 10, 1);
+            Console.CursorVisible = true;
+            float cantidad = int.Parse(obtenerNumero(-10)) / 100f;
+            Console.CursorVisible = false;
+
+            DateTime inicioOperacion = DateTime.Now;
+
+            escribirCentrado("Introduzca el dinero en la bandeja");
+            escribirCentrado("(Presiona cualquier tecla)", offsetVertical: 1);
+            escribirCentrado("-------------------------------", offsetVertical: 3);
+            escribirCentrado("|                             |", offsetVertical: 4);
+            escribirCentrado("-------------------------------", offsetVertical: 5);
+
+            while ((DateTime.Now - inicioOperacion).Minutes < 2)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+
+                    cuenta.Deposito(cantidad);
+                    escribirCentrado("Operación exitosa");
+                    escribirCentrado("El saldo depositado se verá reflejado en su siguiente sesión", offsetVertical: 1);
+                    escribirCentrado("Regresando al menú principal", offsetVertical: 2);
+                    Task.Delay(4000).Wait();
+                    return;
+                }
+            }
+
+            escribirCentrado("Operación fallida, no se recibió el efectivo");
+            escribirCentrado("Regresando al menú principal", offsetVertical: 2);
+            Task.Delay(4000).Wait();
+        }
+    }
+}
---
+++ Cuenta.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ATM
+{
+    public class Cuenta
+    {
+        private const string archivo_cuentas = @"Cuentas.txt";
+
+        private readonly int cuenta;
+
+        /// <summary>
+        /// Saldo de la cuenta disponible para retiro (no incluye depositos).
+        /// </summary>
+        public float Saldo
+        {
+            get
+            {
+                if (float.IsNaN(saldo))
+                    saldo = float.Parse(File.ReadAllLines(@"Cuentas.txt")[cuenta].Split(',')[2]);
+                return saldo;
+            }
+            private set
+            {
+                saldo = value;
+            }
+        }
+
+        private float saldo = float.NaN;
+
+        private float saldoDepositos = 0;
+
+        public Cuenta(int cuenta)
+        {
+            this.cuenta = cuenta;
+        }
+
+        /// <summary>
+        /// Verifica que un numero de cuenta esté registrado
+        /// </summary>
+        /// <param name="cuenta">El numero de cuenta del usuario</param>
+        /// <returns><see cref="true"/> si el usuario existe</returns>
+        public static bool VerificarCuenta(string cuenta)
+        {
+            if (string.IsNullOrEmpty(cuenta) || cuenta.Length != 5)
+                return false;
+
+            return File.ReadAllLines(archivo_cuentas).Any(c => c.StartsWith(cuenta));
+        }
+
+        /// <summary>
+        /// Verifica que un numero de cuenta esté registrado y accede mediante un pin
+        /// </summary>
+        /// <param name="cuenta">El numero de cuenta del usuario</param>
+        /// <param name="pin">PIN del usuario para verificar cuenta</param>
+        /// <returns>Una <see cref="Cuenta"/> para realizar operaciones bancarias</returns>
+        public static Cuenta VerificarCuenta(string cuenta, string pin)
+        {
+            if (string.IsNullOrEmpty(cuenta) || cuenta.Length != 5 || string.IsNullOrEmpty(pin))
+                return null;
+
+            var cuentas = File.ReadAllLines(archivo_cuentas);
+
+            for (int i = 0; i < cuentas.Length; i++)
+            {
+                if (cuentas[i].Substring(0, 5) == cuenta && cuentas[i].Split(',')[1] == pin)
+                    return new Cuenta(i);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Retira <see="cantidad"> de la cuenta de usuario
+        /// </summary>
+        /// <param name="cantidad">Cantidad a ser retirada</param>
+        /// <returns>Si el retiro fue exitoso</returns>
+        public bool Retiro(float cantidad)
+        {
+            if (cantidad <= Saldo)
+            {
+                Saldo -= cantidad;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Deposita <see="cantidad"> a la cuenta de usuario
+        /// Notese que este saldo solo se verá reflejado despues de finalizada la sesión, conforme a la nota del punto 5 accion 3 del documento de especificaciones.
+        /// </summary>
+        /// <param name="cantidad">Cantidad a ser depositada</param>
+        public void Deposito(float cantidad)
+        {
+            saldoDepositos += cantidad;
+        }
+
+        public void CerrarSesion()
+        {
+            var cuentas = File.ReadAllLines(archivo_cuentas);
+            cuentas[cuenta] = cuentas[cuenta].Substring(0, 12) + (Saldo + saldoDepositos);
+            File.WriteAllLines(archivo_cuentas, cuentas);
+        }
+
+        ~Cuenta()
+        {
+            CerrarSesion();
+        }
+    }
+}
